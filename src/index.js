3ec466aacba9916b4c9935cf97bcddb104b7ed7b
Adrian Wojtczak <117528537+adrianwojtczak@users.noreply.github.com>
2023-04-23 00:05:04 +0200

    add search

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+searchBox.addEventListener(
+  'input',
+  debounce(async ev => {
+    const countries = await fetchCountries(ev.target.value);
+
+    if (countries.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific name.'
+      );
+    } else if (countries.length > 1 && countries.length <= 10) {
+      countryList.innerHTML = countries
+        .map(
+          country =>
+            `<li><img height=16 src=${country.flags.png}> ${country.name.common}</li>`
+        )
+        .join('');
+    } else {
+      countryList.innerHTML = '';
+    }
+
+    if (countries.length === 1) {
+      countryInfo.innerHTML = countries
+        .map(
+          country =>
+            `<p><img height=24 src=${country.flags.png}> ${
+              country.name.common
+            }</p>
+            <p>Capital: ${country.capital}</p>
+            <p>Population: ${country.population}</p>
+            <p>Languages: ${Object.values(country.languages).join(', ')}</p>`
+        )
+        .join('');
+    } else {
+      countryInfo.innerHTML = '';
+    }
+
+    console.log(countries);
+  }, DEBOUNCE_DELAY)
+);
